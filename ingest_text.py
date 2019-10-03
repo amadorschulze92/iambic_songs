4918ebcff14786c8912c25029bfdf61fd0b4e836
Michael Schulze <mschulze@Michaels-MacBook-Pro.local>
2019-10-03 13:59:13 -0700

    encoding

--- ingest_text.py
+++ ingest_text.py
@@ -1,10 +1,11 @@
 from functions import PoetryParser
 import pandas as pd
+import sys
 
 pp = PoetryParser()
 
 author="taylor swift"
-df_ts = pd.read_csv("taylor_swift_lyrics.csv")
+df_ts = pd.read_csv("taylor_swift_lyrics.csv", encoding='latin1')
 
 
 text_list, meter_list = pp.text_line_parser(df_ts["lyrics"])
