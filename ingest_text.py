cf97b6df6bf98df534f06b0258dd765695255537
Michael Schulze <mschulze@Michaels-MacBook-Pro.local>
2019-10-03 14:14:48 -0700

    key error

--- ingest_text.py
+++ ingest_text.py
@@ -8,7 +8,7 @@ author="taylor swift"
 df_ts = pd.read_csv("taylor_swift_lyrics.csv", encoding='latin1')
 
 
-text_list, meter_list = pp.text_line_parser(df_ts["lyrics"])
+text_list, meter_list = pp.text_line_parser(df_ts["lyric"])
 syllable_inflection_columns, word_list_column, sonnet_num_list, author_list, polarity_list, subjectivity_list = pp.text_to_df(text_list, meter_list, author)
 austen_text_df = pp.create_dataframe(syllable_inflection_columns, word_list_column, sonnet_num_list, author_list, polarity_list, subjectivity_list)
 
