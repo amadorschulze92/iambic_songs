66a02e9af8755b4bc9f8e8f04761067363e8ccd8
mundyreimer <moreimer@dons.usfca.edu>
2019-10-03 14:33:15 -0700

    add textblob import

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import re
 from math import ceil
-# from textblob import TextBlob
+from textblob import TextBlob
 # import cPickle
 
 def clean_words(words, verbose=True):
