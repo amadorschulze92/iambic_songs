b1645cc302c8dee4f1e1794ee3dd1a77686816fe
Michael Schulze <mschulze@Michaels-MacBook-Pro.local>
2019-10-03 15:13:49 -0700

    do the commandline values

--- ingest_text.py
+++ ingest_text.py
@@ -2,11 +2,10 @@ from functions import *
 import pandas as pd
 import sys
 
-author="taylor swift"
-# df_ts = pd.read_csv("taylor_swift_lyrics.csv", encoding='latin1')
-# for dev work use the smaller data set below:
-df_ts = pd.read_csv("small_taylor_swift_lyrics.csv", encoding='latin1')
+my_file = sys.argv[1]
+author = sys.argv[2]
 
+df_ts = pd.read_csv(my_file, encoding='latin1')
 
 text_list, meter_list = text_line_parser(df_ts["lyric"])
 syllable_inflection_columns, word_list_column, sonnet_num_list, author_list, polarity_list, subjectivity_list = text_to_df(text_list, meter_list, author)
