ac7c2320a1eb6c34b8ab1e62ca21f6abf2eb1b4d
Michael Schulze <mschulze@Michaels-MacBook-Pro.local>
2019-10-11 11:54:46 -0700

    change dataframe var name

--- ingest_text.py
+++ ingest_text.py
@@ -5,9 +5,9 @@ import sys
 my_file = sys.argv[1]
 author = sys.argv[2]
 
-df_ts = pd.read_csv(my_file, encoding='latin1')
+df = pd.read_csv(my_file, encoding='latin1')
 
-text_list, meter_list = text_line_parser(df_ts["lyric"])
+text_list, meter_list = text_line_parser(df["lyric"])
 syllable_inflection_columns, word_list_column, sonnet_num_list, author_list, polarity_list, subjectivity_list = text_to_df(text_list, meter_list, author)
 text_df = create_dataframe(syllable_inflection_columns, word_list_column, sonnet_num_list, author_list, polarity_list, subjectivity_list)
 
